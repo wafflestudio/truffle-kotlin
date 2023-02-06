1262c7d9a338124968214834d2551dd649745dcb
Davin Byeon (Dan) <bdv111@snu.ac.kr>
2023-02-06 08:43:12 +0100

    GitHub Actions and Gradle for Publishing Snapshot and Release (#1)
    * publish, lint and test
    
    * rm println
    
    * rerun-tasks
    
    * Update publish-release.yml
    
    * get authToken in gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.ByteArrayOutputStream
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.util.Properties
 
 plugins {
     id("org.springframework.boot") version "3.0.1" apply false
@@ -6,6 +10,7 @@ plugins {
     kotlin("jvm") version "1.8.0"
     kotlin("plugin.spring") version "1.8.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("maven-publish")
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_17
@@ -45,6 +50,40 @@ allprojects {
         testImplementation("org.springframework.boot:spring-boot-starter-webflux")
     }
 
+    publishing {
+        val authToken = ByteArrayOutputStream().use {
+            runCatching {
+                exec {
+                    commandLine = (
+                        "aws codeartifact get-authorization-token " +
+                            "--domain wafflestudio --domain-owner 405906814034 " +
+                            "--query authorizationToken --region ap-northeast-1 --output text"
+                        ).split(" ")
+                    standardOutput = it
+                }
+            }
+            it.toString()
+        }
+
+        repositories {
+            maven {
+                version = properties["version"]!!
+                name = "wafflestudio"
+                url = uri("https://wafflestudio-405906814034.d.codeartifact.ap-northeast-1.amazonaws.com/maven/truffle-kotlin/")
+                credentials {
+                    username = "aws"
+                    password = authToken
+                }
+            }
+        }
+
+        publications {
+            register<MavenPublication>("truffle-kotlin") {
+                from(components["java"])
+            }
+        }
+    }
+
     tasks.withType<KotlinCompile> {
         kotlinOptions {
             freeCompilerArgs = listOf("-Xjsr305=strict")
@@ -54,5 +93,20 @@ allprojects {
 
     tasks.withType<Test> {
         useJUnitPlatform()
+    }
+}
+
+task("updateVersion") {
+    properties["releaseVersion"]?.let { releaseVersion ->
+        val newSnapshotVersion = (releaseVersion as String).split(".").let {
+            "${it[0]}.${it[1].toInt() + 1}.0-SNAPSHOT"
+        }
+
+        val file = File(rootDir, "gradle.properties")
+        val prop = Properties().apply { load(FileInputStream(file)) }
+        if (prop.getProperty("version") != newSnapshotVersion) {
+            prop.setProperty("version", newSnapshotVersion)
+            prop.store(FileOutputStream(file), null)
+        }
     }
 }
